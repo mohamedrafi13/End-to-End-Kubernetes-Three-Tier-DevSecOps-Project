15d7d669ff4a66c63d02fe9cb7b21f827c39a869
developer1 <rafi.huta@gmail.com>
2024-02-11 17:35:41 +0530

    modified backend & gather files

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "demo-rafi-sse"
     region         = "ap-south-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "Table-Lock"
     encrypt        = true
   }
   required_version = ">=0.13.0"
